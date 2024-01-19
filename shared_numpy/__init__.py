54350c5413018b641cc447213866d7a521d9435b
Andrii <andrii.litvynchuk@leiainc.com>
2024-01-19 12:33:19 +0000

    no _all_

--- shared_numpy/__init__.py
+++ shared_numpy/__init__.py
@@ -1,10 +1,7 @@
 from .queue import Queue
 from .reduction import init_reduction
-from .shared_memory import ShareableList, SharedMemory
-from .shared_numpy import SharedNDArray, array, from_array, ndarray
-
-
-__all__ = ["Queue", "from_arry", "array", "ndarray", "SharedMemory", "ShareableList", "SharedNDArray"]
+from .shared_memory import *
+from .shared_numpy import *
 
 
 init_reduction()
